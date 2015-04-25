893e8901287f7e76360ef0729911120a835870e1
Paul Gibbs <paul@byotos.com>
2015-04-25 21:33:47 +0100

    Add initial modinfo.lua
    Mod name / description details, etc.

---
+++ modinfo.lua
@@ -0,0 +1,14 @@
+name        = "DJPaul's Sort Inventory"
+description = "Sensible inventory auto-sorting for Don't Starve Together."
+author      = "DJPaul"
+version     = "0.1"
+forumthread = ""
+
+api_version                = 10  --DST api version
+dont_starve_compatible     = false
+reign_of_giants_compatible = false
+dst_compatible             = true
+priority                   = 0  -- Relative mod load order
+
+client_only_mod         = true
+all_clients_require_mod = false
