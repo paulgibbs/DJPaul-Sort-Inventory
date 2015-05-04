aeb835e4558333d83624d83f89f5ad39dbdbbaf8
Paul Gibbs <paul@byotos.com>
2015-05-04 19:09:56 +0100

    Add config option for number of torches sorted.
    Not implemented in the sort logic yet.

--- modinfo.lua
+++ modinfo.lua
@@ -12,3 +12,19 @@ priority                   = 0  -- Relative mod load order
 
 client_only_mod         = true
 all_clients_require_mod = false
+
+configuration_options = {
+	{
+		default = 2,
+		label   = "Number of torches",
+		name    = "dsiLightCount",
+		options = (function()
+			local values = {}
+			for i = 1, 10 do
+				values[#values+1] = { description = i + 0, data = i }
+			end
+
+			return values
+		end)()
+	},
+}
