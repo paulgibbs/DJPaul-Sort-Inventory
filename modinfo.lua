4a467eddaec088179b2faa76c938a3af280631d6
Paul Gibbs <djpaul@gmail.com>
2015-11-19 22:15:55 +0000

    Bump version

--- modinfo.lua
+++ modinfo.lua
@@ -1,7 +1,7 @@
 name        = "DJPaul's Sort Inventory"
 description = "Automatically stacks and sorts your inventory into a sensible order."
 author      = "Paul Gibbs (DJPaul)"
-version     = "1.4"
+version     = "1.5"
 forumthread = "/topic/54138-new-mod-djpauls-sort-inventory/"
 
 api_version                = 10  -- DST api version
